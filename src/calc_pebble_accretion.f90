3f182495f3c84970100418a0dbeeca7cbf1254aa
Duncan Forgan <dhf3@st-andrews.ac.uk>
2017-12-11 15:05:20 +0000

    Code runs OK - still an issue with units somewhere, getting huge mdots

--- src/calc_pebble_accretion.f90
+++ src/calc_pebble_accretion.f90
@@ -40,7 +40,8 @@ PROGRAM calc_pebble_accretion
   logical :: inner_radius
   logical :: outer_radius
 
-  character(7) :: mdot_char
+  character(3) :: imdot_char
+  character(14) :: mdot_char
   character(100) ::inputfile, outputprefix, outputlog, output_mdotfile
 
   ! Get input parameters and set up header:
@@ -71,9 +72,9 @@ PROGRAM calc_pebble_accretion
   print*, 'Outputting pebble accretion data to files of prefix ', outputprefix
 
 
-  outputlog = trim(outputprefix)//"_log.pebble"
+  outputlog = trim(outputprefix)//".log"
   print*, "Log data for all gas accretion rates to be outputted to ", trim(outputlog)
-  print*, "Data for each accretion rate to be outputted to files of form", trim(outputlog)//"mdot_+++.pebble"
+  print*, "Data for each accretion rate to be outputted to files of form ", trim(outputlog)//".mdot.<num>"
 
   !******************************************
   ! 1. Read in disc and recompute essential gas  properties
@@ -86,22 +87,24 @@ PROGRAM calc_pebble_accretion
   read(10,*) nrad, nmdot, rmin,rmax,mdotmin,mdotmax,Mstar, metallicity,&
        gamma_sigma,gamma_omega,irrchoice,Q_irr,T_irr
 
+  mstar = mstar*umass
+  print*, 'There are ', nrad, ' radii in the input file'
+
   allocate(r(nrad))
   allocate(sigma(nrad))
   allocate(cs(nrad))
   allocate(H(nrad))
   allocate(rhogas(nrad))
   allocate(omega(nrad))
+  allocate(alpha(nrad))
   allocate(eta(nrad))
   allocate(vrpeb(nrad))
   allocate(stream_unstable(nrad))
 
-
-
   ! Find index of maximum radius for pebble accretion (npebrad)
   dr = (rmax-rmin)/REAL(nrad)
   !dr = dr*udist
-  imax_peb = int(rmax_peb/dr) + 1
+  npebrad = int(rmax_peb/dr) + 1
 
   ! Write header for log output file
   OPEN(20,file=outputlog,status='unknown')
@@ -112,13 +115,13 @@ PROGRAM calc_pebble_accretion
   ! Loop over accretion rates
   DO imdot = 1, nmdot
 
-
      r(:) = 0.0
      sigma(:) = 0.0
      cs(:) = 0.0
      H(:) = 0.0
      rhogas(:) = 0.0
      omega(:) = 0.0
+     alpha(:) = 0.0
      eta(:) = 0.0
      vrpeb(:) = 0.0
      stream_unstable(:) = 0
@@ -129,15 +132,20 @@ PROGRAM calc_pebble_accretion
         read(10,*) r(irad), mdotvisc, qratio, sigma(irad), cs(irad),omega(irad),&
              T,betac,alpha(irad)
 
-        ! TODO - check units
-        H(irad) = cs(irad)/omega(irad)
+        !print*, r(irad), mdotvisc, qratio, sigma(irad), cs(irad),omega(irad),&
+        !     T,betac,alpha(irad)
+
+
+        ! Be careful with units here
+        H(irad) = (cs(irad)/omega(irad)) ! in units of cm
         rhogas(irad) = sigma(irad)/(2.0*H(irad))
-        !r(irad) = r(irad)*udist
-        
+        r(irad) = r(irad)*udist
+                
         ! Calculate pressure gradient and eta - sub Keplerian parameter
 
         if(irad>1) then
-           dlogrhodr = (log(rhogas(irad)*cs(irad)*cs(irad)) - log(rhogas(irad)*cs(irad)*cs(irad)))/(log(r(irad) - log(r(irad-1))))
+           dlogrhodr = (log(rhogas(irad)*cs(irad)*cs(irad)) - log(rhogas(irad-1)*cs(irad-1)*cs(irad-1)))/&
+                (log(r(irad) - log(r(irad-1))))
         else
            dlogrhodr = 0.0
         endif
@@ -145,7 +153,7 @@ PROGRAM calc_pebble_accretion
         eta(irad) = -0.5*(H(irad)*H(irad))/(r(irad)*r(irad)) * dlogrhodr
 
         ! Radial velocity of pebbles
-        vrpeb(irad) = -2.0*eta(irad)*omega(irad)*r(irad)*tstop/(1.0 + tstop*tstop)
+        vrpeb(irad) = 2.0*eta(irad)*omega(irad)*r(irad)*tstop/(1.0 + tstop*tstop)
 
      enddo
 
@@ -154,14 +162,13 @@ PROGRAM calc_pebble_accretion
      ! 2. Now compute pebble accretion properties as a function of rpeb
      !******************************************************************
 
-    
-
      ! Specify outputfile for this accretion rate
 
-     write(mdot_char,'(e5.2)') mdotvisc
-     output_mdotfile = trim(outputprefix)//"mdot_"//trim(mdot_char)//".pebble"
+     write(imdot_char, '(I3.3)') imdot
+     output_mdotfile = trim(outputprefix)//".mdot."//trim(imdot_char)
 
      open(30, file=output_mdotfile, status='unknown')
+     write(30,*) mdotvisc
 
      ! Variables store maximum value at this accretion rate
      planet_accretemax = 0.0
@@ -169,10 +176,12 @@ PROGRAM calc_pebble_accretion
      mcross_accretemax = 0.0
      eff_accretemax = 0.0
 
+     ! convert mdot into cgs
      mdotvisc = mdotvisc*umass/3.15e7
 
      ! Now loop again over radius, where radius now refers to rpeb
 
+     print*, 'Number of pebble radii' , npebrad
      do ipebrad =1,npebrad
 
         rpeb = r(ipebrad)
@@ -190,8 +199,7 @@ PROGRAM calc_pebble_accretion
         ! Compute mdotpebble
         mdotpebble = 2.0*pi*rpeb*rdotpeb*zpeb*sigma(ipebrad)
 
-
-
+!        print*, mdotvisc*3.15e7/umass, mstar/umass, rpeb/udist, tpeb/3.15e7, rdotpeb*3.15e7/udist, mdotpebble*3.15e7/umass
         !************************************************
         ! 2b Find regions where streaming instability active
         !************************************************
@@ -199,13 +207,16 @@ PROGRAM calc_pebble_accretion
         stream_unstable(:) = 0
         inner_radius = .false.
         outer_radius = .false.
+        rmin_unstable = 0.0
+        rmax_unstable = 0.0
 
-        do jrad =1,ipebrad
+        do jrad =2,ipebrad
 
            Hp_to_Hg = sqrt(tstop/alpha(jrad))
            ! Compute rhop/rhog interior to pebble radius
            rhop_rhog = mdotpebble/(Hp_to_Hg*sigma(jrad)*2.0*pi*r(jrad)*vrpeb(jrad))
 
+           !print*, jrad, rhop_rhog, Hp_to_Hg, sigma(jrad), vrpeb(jrad)
            ! If rhop/rhog >1, mark this radius as streaming unstable region
            
            if(rhop_rhog>1.0) then
@@ -227,6 +238,15 @@ PROGRAM calc_pebble_accretion
 
         enddo
 
+!!$        print*, 'out'
+!!$        ! If streaming zone goes right to pebble front, make sure outer radius is found
+!!$        if (inner_radius.eqv. .true. .and. outer_radius.eqv..false.) then
+!!$           print*, 'here'
+!!$           rmax_unstable = r(ipebrad)
+!!$           irmax_unstable = ipebrad
+!!$           outer_radius = .true.
+!!$        endif
+
         !****************************************************
         ! 3 Calculate expected core accretion rates
         !****************************************************
@@ -238,6 +258,7 @@ PROGRAM calc_pebble_accretion
         ! Compute surface density of pebbles in here
         sigma_p = mdotpebble/(2.0*pi*r(irmin_unstable)*vrpeb(irmin_unstable))
 
+        print*, sigma(irmin_unstable), sigma_p
         ! Compute Pebble Accretion Rate for M= 1 Mjup
         ! Mdot = 2 R_H^2 omega tstop *sigma p
 
@@ -270,14 +291,17 @@ PROGRAM calc_pebble_accretion
         ! Write to file for this rpeb
         ! mdot rpeb --> mdotpebble, r1, r2, Mcross, Mpl
         
-        write(30,*) mdotvisc, rpeb,tpeb, rdotpeb,  mdotpebble, rmin_unstable, rmax_unstable, mcross, planet_pebaccrete, eff_pebble
+        write(30,*) mdotvisc*3.15e7/umass, rpeb/udist,tpeb/3.15e7, rdotpeb*3.15e7/udist, &
+             mdotpebble*3.15e7/umass, rmin_unstable/udist, rmax_unstable/udist, &
+             mcross/mjup, planet_pebaccrete*3.15e7/mjup, eff_pebble
 
      enddo
      close(30)
 
      ! Now write log file for this mdot
      ! mdot: maximum mdotp, rg(max mdotp), Mcross(max mdotp), efficiency
-     write(20,*) mdotvisc, rpeb_accretemax, tpeb_accretemax, planet_accretemax, mcross_accretemax, eff_accretemax
+     write(20,*) mdotvisc, rpeb_accretemax, tpeb_accretemax, planet_accretemax, &
+          mcross_accretemax, eff_accretemax
 
   enddo
         
